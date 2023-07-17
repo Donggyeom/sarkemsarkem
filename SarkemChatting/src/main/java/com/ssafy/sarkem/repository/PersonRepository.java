59ca0d25ec8d3f586d59bdf0d49a23c63573b40e
김동겸 <eastcrtsy@gmail.com>
2023-07-17 17:51:16 +0900

    Revert "feat: Redis Sample Project"
    This reverts commit e2a055d760432e532eee3549e77a3927f3188f1b

--- SarkemChatting/src/main/java/com/ssafy/sarkem/repository/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.sarkem.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.ssafy.sarkem.entity.Person;
-
-public interface PersonRepository extends CrudRepository<Person, String>{
-
-}
--- sample/SarkemChatting/src/main/java/com/ssafy/sarkem/entity/School.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.sarkem.entity;
-
-import lombok.Data;
-
-@Data
-public class School {
-	private String name;
-}
