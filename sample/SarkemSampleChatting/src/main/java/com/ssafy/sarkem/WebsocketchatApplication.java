61b3ecbb29a1ad72bb3b5ac5ea0cba4d9d3e93bc
박시원 <tldnjs324@naver.com>
2023-07-18 13:21:46 +0900

    feat: Added Sample Chatting Project

---
+++ sample/SarkemSampleChatting/src/main/java/com/ssafy/sarkem/WebsocketchatApplication.java
@@ -0,0 +1,11 @@
+package com.ssafy.sarkem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketchatApplication {
+	 public static void main(String[] args) {
+	        SpringApplication.run(WebsocketchatApplication.class, args);
+	    }
+}
