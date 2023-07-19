7759c2603c686ddcc6ee4b79e7ec96a25e80df4c
Kim Donggyeom <eastcrtsy@gmail.com>
2023-07-19 13:58:21 +0900

    Feat: 프로젝트 폴더 생성

---
+++ Sarkem/backend/src/main/java/com/example/sarkem/SarkemApplication.java
@@ -0,0 +1,13 @@
+package com.example.sarkem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarkemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SarkemApplication.class, args);
+	}
+
+}
