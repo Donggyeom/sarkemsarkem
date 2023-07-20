50800cd134318f000935112a74e47c68e540b962
김민식 <sakmsik98@naver.com>
2023-07-20 17:46:34 +0900

    feat : sample case

---
+++ sample/sarkem_B/backend/src/test/java/com/example/sarkem/model/enums/GameRole.java
@@ -0,0 +1,12 @@
+package com.example.sarkem.model.enums;
+
+public enum GameRole {
+	CAT,
+	SARK,
+	DOCTOR,
+	POLICE,
+	DETECTIVE,
+	GANGSTER,
+	PSYCOLOGIST,
+	OBSERVER;
+}
---
+++ sample/sarkem_B/backend/src/test/java/com/example/sarkem/model/request/GameSessionPostReq.java
@@ -0,0 +1,5 @@
+package com.example.sarkem.model.request;
+
+public class GameSessionPostReq {
+
+}
