cb193c1db622804cc30d4369c17c873909aba9fa
김민식 <sakmsik98@naver.com>
2023-07-21 14:41:07 +0900

    fix : Swagger

--- Sarkem/backend/src/main/java/com/a702/sarkem/config/SwaggerConfig.java
+++ Sarkem/backend/src/main/java/com/a702/sarkem/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.a702.sarkem.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -11,22 +12,24 @@ import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfig {
-
-    @Bean
-    public Docket restAPI() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.hobbybee"))
-                .paths(PathSelectors.any())
-                .build();
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("hobbybee Spring Boot REST API")
-                .version("1.0.0")
-                .description("취미 생활 커뮤니티 hobbybee의 swagger api 입니다.")
-                .build();
-    }
+	
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.a702.sarkem.controller"))
+				.paths(PathSelectors.ant("/api*/**"))
+				.build()
+				.apiInfo(apiInfo());
+	}
+	
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("Sarkem")
+				.description("Sarkem")
+				.version("0.1")
+				.build();
+	}
+	
+	
 }
