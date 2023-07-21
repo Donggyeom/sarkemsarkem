31196551fe77f23927e18f757dfa404fedafd2d6
김민식 <sakmsik98@naver.com>
2023-07-21 17:43:16 +0900

    feat : GameController 작성

--- Sarkem/backend/src/main/java/com/a702/sarkem/controller/MainController.java
+++ Sarkem/backend/src/main/java/com/a702/sarkem/controller/MainController.java
@@ -1,16 +1,95 @@
 package com.a702.sarkem.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.a702.sarkem.service.GameManager;
+
+import io.openvidu.java.client.Connection;
+import io.openvidu.java.client.ConnectionProperties;
+import io.openvidu.java.client.OpenVidu;
+import io.openvidu.java.client.OpenViduHttpException;
+import io.openvidu.java.client.OpenViduJavaClientException;
+import io.openvidu.java.client.Session;
+import io.openvidu.java.client.SessionProperties;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RequestMapping("/api/gamesession")
 @RestController
+@Slf4j
+@RequiredArgsConstructor
 public class MainController {
+
+    @Value("${OPENVIDU_URL}")
+    private String OPENVIDU_URL;
+
+    @Value("${OPENVIDU_SECRET}")
+    private String OPENVIDU_SECRET;
+
+    private OpenVidu openvidu;
+
+    @PostConstruct
+    public void init() {
+        this.openvidu = new OpenVidu(OPENVIDU_URL, OPENVIDU_SECRET);
+    }
 	
+    Map paramMap = new HashMap<>();
+    
+    
 	// gameManager
-	
-	// 방 생성  createGameRoom
-	
-	// 방 획득  retrieveGameRoom
-	
-	// 방 접속  connectRoom(@RequestParam String roomId, Player player)
+	private final GameManager gameManager;
+
+	// 방 생성 createGameRoom
+    @PostMapping("/api/game")
+    public ResponseEntity<String> createGameRoom(@RequestBody(required = false) Map<String, Object> params)
+            throws OpenViduJavaClientException, OpenViduHttpException {
+    	//Openvidu Session
+        SessionProperties properties = SessionProperties.fromJson(params).build();
+        Session session = openvidu.createSession(properties);
+        //Game Session
+        gameManager.createGameRoom(session.getSessionId());
+        String token = getConnectionToken(session, paramMap);
+        return new ResponseEntity<>(session.getSessionId(), HttpStatus.OK);
+    }
+	// 방 획득 retrieveGameRoom
+
+	// 방 접속 connectRoom(@RequestParam String roomId, Player player)
+    @PutMapping("/api/game/{roomId}")
+    public ResponseEntity<String> connectRoom(@PathVariable("sessionId") String roomId,
+                                                   @RequestBody(required = false) Map<String, Object> params)
+            throws OpenViduJavaClientException, OpenViduHttpException {
+        Session session = openvidu.getActiveSession(roomId);
+        if (session == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        
+        return new ResponseEntity<>(getConnectionToken(session, params), HttpStatus.OK);
+    }
+    
+    public String getConnectionToken(Session session, Map<String, Object> params) throws OpenViduJavaClientException, OpenViduHttpException{
+    	ConnectionProperties properties = ConnectionProperties.fromJson(params).build();
+        Connection connection = session.createConnection(properties);
+        return connection.getToken();
+    }
+    
+    
 	// gameManager.getGameRoom(roomId).connect(player)
+    
+    
+    
 }
