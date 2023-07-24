433d3efb30adcd0d6a5c7e619cbee0b4ef44c94f
Kim Donggyeom <eastcrtsy@gmail.com>
2023-07-24 15:50:15 +0900

    Fix: 실행을 위한 임시 주석 처리

--- Sarkem/backend/src/main/java/com/a702/sarkem/redis/NightVoteSubscriber.java
+++ Sarkem/backend/src/main/java/com/a702/sarkem/redis/NightVoteSubscriber.java
@@ -21,42 +21,36 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Service
 public class NightVoteSubscriber {
-	  private final ObjectMapper objectMapper;
-	  private final SimpMessagingTemplate template;
-	  private final GamePublisher redisPublisher;
-	  private final GameManager gameManager;
-	  private final ChannelTopic topicStartFin;
-	  private final RedissonClient redissonClient;
-	  private static final String KEY = "GameSession";
-	  
-	  public void sendMessage(String message) {
-		    try {
-		      NightVoteMessage nightVoteMessage = objectMapper.readValue(message, NightVoteMessage.class);
-		      String roomId = nightVoteMessage.getRoomId();
-		      Map<GameRole, String> roleVote = nightVoteMessage.getRoleVoteResult();
-
-		      
-		      GameSession gameSession = null;
-		      String deadPlayerId = roleVote.get(GameRole.SARK); // 삵이 투표해서 죽은 플래이어 아이디
-		      String protectedPlayerId = roleVote.get(GameRole.DOCTOR); // 의사가 투표해서 지켜진 플래이어 아이디
-		      String suspectPlayerId = roleVote.get(GameRole.POLICE); // 경찰이 투표해서 조사받을 플래이어 아이디
-		      String slientPlayerId = roleVote.get(GameRole.ACHI); // 냥아치가 투표해서 조용해야 할 플래이어 아이디
-
-		      log.info("Room {} NightVote deadPlayer: {}", roomId, deadPlayerId);
-		      log.info("Room {} NightVote protectedPlayer: {}", roomId, protectedPlayerId);
-		      log.info("Room {} NightVote suspectPlayer: {}", roomId, suspectPlayerId);
-		      log.info("Room {} NightVote slientPlayer: {}", roomId, slientPlayerId);
-
-		      // 의사가 살렸을 경우 부활
-		      if (deadPlayerId != null && deadPlayerId.equals(protectedPlayerId)) {
-		        deadPlayerId = null;
-		      }
-
-		      
-		    } catch (JsonProcessingException e) {
-		      e.printStackTrace();
-		    }
-		  }
-
-		  
+	/*
+	 * private final ObjectMapper objectMapper; private final SimpMessagingTemplate
+	 * template; private final GamePublisher redisPublisher; private final
+	 * GameManager gameManager; private final ChannelTopic topicStartFin; private
+	 * final RedissonClient redissonClient; private static final String KEY =
+	 * "GameSession";
+	 * 
+	 * public void sendMessage(String message) { try { NightVoteMessage
+	 * nightVoteMessage = objectMapper.readValue(message, NightVoteMessage.class);
+	 * String roomId = nightVoteMessage.getRoomId(); Map<GameRole, String> roleVote
+	 * = nightVoteMessage.getRoleVoteResult();
+	 * 
+	 * 
+	 * GameSession gameSession = null; String deadPlayerId =
+	 * roleVote.get(GameRole.SARK); // 삵이 투표해서 죽은 플래이어 아이디 String protectedPlayerId
+	 * = roleVote.get(GameRole.DOCTOR); // 의사가 투표해서 지켜진 플래이어 아이디 String
+	 * suspectPlayerId = roleVote.get(GameRole.POLICE); // 경찰이 투표해서 조사받을 플래이어 아이디
+	 * String slientPlayerId = roleVote.get(GameRole.ACHI); // 냥아치가 투표해서 조용해야 할 플래이어
+	 * 아이디
+	 * 
+	 * log.info("Room {} NightVote deadPlayer: {}", roomId, deadPlayerId);
+	 * log.info("Room {} NightVote protectedPlayer: {}", roomId, protectedPlayerId);
+	 * log.info("Room {} NightVote suspectPlayer: {}", roomId, suspectPlayerId);
+	 * log.info("Room {} NightVote slientPlayer: {}", roomId, slientPlayerId);
+	 * 
+	 * // 의사가 살렸을 경우 부활 if (deadPlayerId != null &&
+	 * deadPlayerId.equals(protectedPlayerId)) { deadPlayerId = null; }
+	 * 
+	 * 
+	 * } catch (JsonProcessingException e) { e.printStackTrace(); } }
+	 * 
+	 */
 }
--- Sarkem/backend/src/main/java/com/a702/sarkem/repository/VoteRedisRepository.java
+++ Sarkem/backend/src/main/java/com/a702/sarkem/repository/VoteRedisRepository.java
@@ -19,81 +19,49 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Repository
 public class VoteRedisRepository {
-	private final RedisTemplate<String, Vote> redisTemplate;
-	private HashOperations<String, String, Vote> opsHashVote;
-	private static final String key = "Vote";
-
-	@PostConstruct
-	private void init() {
-		opsHashVote = redisTemplate.opsForHash();
-	}
-
-	public Map<String, Vote> startVote(List<String> players, PhaseType phase) {
-		Map<String, Vote> voteResult = new HashMap<String, Vote>();
-
-		players.forEach((playerId) -> {
-			Vote vote;
-			if (!isExist(playerId)) {
-				vote = Vote.builder(playerId, phase);
-			} else {
-				vote = getVote(playerId);
-			}
-
-			updateVote(playerId, vote);
-		});
-		return voteResult;
-	}
-
-	public void removeVote(String playerId) {
-		deleteVote(playerId);
-	}
-
-	public boolean isExist(String playerId) {
-		return opsHashVote.hasKey(key, playerId);
-	}
-
-	public Vote getVote(String playerId) {
-		return opsHashVote.get(key, playerId);
-	}
-
-	public void vote(String playerId, String vote) {
-		Vote voteDao = getVote(playerId);
-		voteDao.setVote(vote);
-		updateVote(playerId, voteDao);
-	}
-
-	public boolean confirmVote(String playerId) {
-		Vote voteDao = getVote(playerId);
-		if (!voteDao.isConfirm()) {
-			voteDao.setConfirm(true);
-			updateVote(playerId, voteDao);
-			return true;
-		}
-		return false;
-	}
-
-	public Map<String, Vote> getVoteResult(List<String> players) {
-		Map<String, Vote> voteResult = new HashMap<String, Vote>();
-		players.forEach(playerId -> {
-			voteResult.put(playerId, getVote(playerId));
-		});
-		return voteResult;
-	}
-
-	public void endVote(List<String> players, PhaseType phase) {
-		players.forEach((playerId) -> {
-			Vote voteDao = getVote(playerId);
-			if (voteDao.getPhase() == phase) {
-				deleteVote(playerId);
-			}
-		});
-	}
-
-	private void updateVote(String playerId, Vote voteDao) {
-		opsHashVote.put(key, playerId, voteDao);
-	}
-
-	private void deleteVote(String playerId) {
-		opsHashVote.delete(key, playerId);
-	}
+	/*
+	 * private final RedisTemplate<String, Vote> redisTemplate; private
+	 * HashOperations<String, String, Vote> opsHashVote; private static final String
+	 * key = "Vote";
+	 * 
+	 * @PostConstruct private void init() { opsHashVote =
+	 * redisTemplate.opsForHash(); }
+	 * 
+	 * public Map<String, Vote> startVote(List<String> players, PhaseType phase) {
+	 * Map<String, Vote> voteResult = new HashMap<String, Vote>();
+	 * 
+	 * players.forEach((playerId) -> { Vote vote; if (!isExist(playerId)) { vote =
+	 * Vote.builder(playerId, phase); } else { vote = getVote(playerId); }
+	 * 
+	 * updateVote(playerId, vote); }); return voteResult; }
+	 * 
+	 * public void removeVote(String playerId) { deleteVote(playerId); }
+	 * 
+	 * public boolean isExist(String playerId) { return opsHashVote.hasKey(key,
+	 * playerId); }
+	 * 
+	 * public Vote getVote(String playerId) { return opsHashVote.get(key, playerId);
+	 * }
+	 * 
+	 * public void vote(String playerId, String vote) { Vote voteDao =
+	 * getVote(playerId); voteDao.setVote(vote); updateVote(playerId, voteDao); }
+	 * 
+	 * public boolean confirmVote(String playerId) { Vote voteDao =
+	 * getVote(playerId); if (!voteDao.isConfirm()) { voteDao.setConfirm(true);
+	 * updateVote(playerId, voteDao); return true; } return false; }
+	 * 
+	 * public Map<String, Vote> getVoteResult(List<String> players) { Map<String,
+	 * Vote> voteResult = new HashMap<String, Vote>(); players.forEach(playerId -> {
+	 * voteResult.put(playerId, getVote(playerId)); }); return voteResult; }
+	 * 
+	 * public void endVote(List<String> players, PhaseType phase) {
+	 * players.forEach((playerId) -> { Vote voteDao = getVote(playerId); if
+	 * (voteDao.getPhase() == phase) { deleteVote(playerId); } }); }
+	 * 
+	 * private void updateVote(String playerId, Vote voteDao) { opsHashVote.put(key,
+	 * playerId, voteDao); }
+	 * 
+	 * private void deleteVote(String playerId) { opsHashVote.delete(key, playerId);
+	 * }
+	 */
 }
