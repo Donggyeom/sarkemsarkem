c86df7e04cea7251c0a02f9f190c91e20132d5d7
fctm212 <fctnm212@naver.com>
2023-08-02 15:38:51 +0900

    style: font 임시 수정. 확인 필

--- Sarkem/front_ex/src/GameContext.js
+++ Sarkem/front_ex/src/GameContext.js
@@ -58,7 +58,9 @@ const onSocketConnected = () => {
         if (token != sysMessage.playerId) return;
 
         switch (sysMessage.code) {
+          // param에 phase, message
         case "NOTICE_MESSAGE":
+            console.log(sysMessage.param);
             alert(sysMessage.param.message);
             break;
         case "GAME_START":   
