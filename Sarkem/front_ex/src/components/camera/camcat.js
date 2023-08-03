33638f3ceefabf1d100fb1b6594b9718a17b8e8f
김민식 <sakmsik98@naver.com>
2023-08-03 13:18:38 +0900

    feat : faceapi

--- Sarkem/front_ex/src/components/camera/camcat.js
+++ Sarkem/front_ex/src/components/camera/camcat.js
@@ -1,8 +1,38 @@
 import React, { memo, useState, useEffect, useRef } from 'react';
 import camcatImage from '../../img/camcat2.png';
 import OpenViduVideoComponent from './OvVideo';
+import { loadModels, faceMyDetect, stopFace } from '../job/psychologist';
+import styled from 'styled-components';
+
+const Box = styled.div
+  `
+  position: absolute;
+  border: 2px solid red;
+`;
+
 
 const CamCat = (props) => {
+
+  const [running, setRunning] = useState(false);
+  const [intervalId, setIntervalId] = useState(null);
+  const [boxPosition, setBoxPosition] = useState({ x: 0, y: 0, width: 0, height: 0 });
+
+  // const nickName = JSON.parse(streamManager.stream.connection.data).userData;
+  // console.log(props.props.videos[1].video);
+  useEffect(() => {
+    loadModels();
+  }, []);
+
+  const startFaceDetection = () => {
+    const id = faceMyDetect(props.props.videos[1].video, setBoxPosition, running, setRunning);
+    setIntervalId(id);
+  };
+
+  const stopFaceDetection = () => {
+    stopFace(intervalId, setRunning, setBoxPosition);
+  };
+
+
   return (
     <div
       style={{
@@ -20,7 +50,7 @@ const CamCat = (props) => {
 
       <div className="streamcomponent" style={{ flex: 0.2 }}>
         <div style={{ flex: 0.6, justifyContent: 'center' }}>
-          
+
           <OpenViduVideoComponent streamManager={props.props} />
           <img
             src={camcatImage}
@@ -31,14 +61,26 @@ const CamCat = (props) => {
               left: '-1.5%',
               width: '96%',
               height: '40%',
-              overflow: 'visible', 
+              overflow: 'visible',
             }}
           />
+          {running && (
+            <Box
+              style={{
+                left: boxPosition.x,
+                top: boxPosition.y,
+                width: boxPosition.width,
+                height: boxPosition.height,
+              }}
+            />
+          )}
         </div>
 
         <div style={{ flex: 0.4, textAlign: 'center', width: '95%' }}>
           {JSON.parse(props.props.stream.connection.data).userData}
         </div>
+        <button onClick={startFaceDetection}>심리학자 시작</button>
+        <button onClick={stopFaceDetection}>심리학자 종료</button>
       </div>
     </div>
   );
