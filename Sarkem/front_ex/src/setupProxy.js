52f1e65ac8480c1fcec50585bb7e1d720a2c010d
hyunchul1998 <hyunchul1998@naver.com>
2023-08-03 13:30:35 +0900

    Fix: setupProxy 초기화

--- Sarkem/front_ex/src/setupProxy.js
+++ Sarkem/front_ex/src/setupProxy.js
@@ -2,13 +2,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware(['/api', '/ws-stomp'], {
+    createProxyMiddleware('/api', {
       target: 'http://localhost:8081',
       changeOrigin: true,
-      ws: true,
-      router: {
-        '/ws-stomp': 'ws://nginx:80'
-      },
     })
   );
 };
