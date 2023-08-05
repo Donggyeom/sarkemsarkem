1774f210327e7aac3caf017c8747377de6338fcf
임혜진 <ccookie0415@gmail.com>
2023-08-05 18:54:04 +0900

    style : sunsetpage 찬성/반대 변경 --> 미완성

--- Sarkem/front_ex/src/components/buttons/agreeDisagreeButtons.js.js
+++ Sarkem/front_ex/src/components/buttons/agreeDisagreeButtons.js.js
@@ -1,54 +1,27 @@
-// import React from 'react';
-// import styled from 'styled-components';
-// import agreeButtonImage from '../../img/찬성.png';
-// import disagreeButtonImage from '../../img/반대.png';
-
-// const Button = styled.button`
-//   background: none;
-//   border: none;
-//   cursor: pointer;
-// `;
-
-// const AgreeButton = ({ onClick, disabled }) => (
-//   <Button onClick={onClick} disabled={disabled}>
-//     <img src={agreeButtonImage} alt="찬성" />
-//   </Button>
-// );
-
-// const DisagreeButton = ({ onClick, disabled }) => (
-//   <Button onClick={onClick} disabled={disabled}>
-//     <img src={disagreeButtonImage} alt="반대" />
-//   </Button>
-// );
-
-// export { AgreeButton, DisagreeButton };
-
 import React from 'react';
 import styled from 'styled-components';
 import agreeButtonImage from '../../img/찬성.png';
 import disagreeButtonImage from '../../img/반대.png';
 
-const Button = styled.button`
-  background: none;
-  border: none;
-  cursor: pointer;
-`;
 
-const SmallButton = styled(Button)`
-  width: 20%;
-  height: 100%;
+const SmallButton = styled.button`
+  padding: 0;
+  background: none;
+  cursor: pointer;
+  border: none;
 `;
 
 const AgreeButton = ({ onClick, disabled }) => (
   <SmallButton onClick={onClick} disabled={disabled}>
-    <img src={agreeButtonImage} alt="찬성" style={{ width: '100%', height: '100%' }} />
+    <img src={agreeButtonImage} alt="찬성" style={{ width: '50%', height: '100%' }} />
   </SmallButton>
 );
 
 const DisagreeButton = ({ onClick, disabled }) => (
   <SmallButton onClick={onClick} disabled={disabled}>
-    <img src={disagreeButtonImage} alt="반대" style={{ width: '100%', height: '100%' }} />
+    <img src={disagreeButtonImage} alt="반대" style={{ width: '50%', height: '100%' }} />
   </SmallButton>
 );
+
 
 export { AgreeButton, DisagreeButton };
