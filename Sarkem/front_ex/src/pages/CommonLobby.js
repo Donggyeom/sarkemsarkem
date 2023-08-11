de15d0f9d5bf62e698fa6a450f9a7ab36798206d
hyunchul1998 <hyunchul1998@naver.com>
2023-08-11 10:19:52 +0900

    Fix: 게임 초대 링크 수정

--- Sarkem/front_ex/src/pages/CommonLobby.js
+++ Sarkem/front_ex/src/pages/CommonLobby.js
@@ -199,7 +199,7 @@ const CommonLobby = ()=>{
   
   // Function to handle the click event when the user wants to invite others
   const handleInviteClick = async () => {
-    await navigator.clipboard.writeText("localhost:3000/"+roomSession.roomId).then(alert("게임 링크가 복사되었습니다."));
+    await navigator.clipboard.writeText("i9a702.p.ssafy.io/"+roomSession.roomId).then(alert("게임 링크가 복사되었습니다."));
     console.log('Invite functionality for hosts');
   };
   
