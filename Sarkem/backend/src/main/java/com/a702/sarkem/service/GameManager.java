a9d35bc9b8a6f868ab3b7260d14ff9a7e667f5d2
ParkSiwon <tldnjs324@naver.com>
2023-08-12 16:48:25 +0900

    feat: lobby popup... 근데 한번만 뜸..

--- Sarkem/backend/src/main/java/com/a702/sarkem/service/GameManager.java
+++ Sarkem/backend/src/main/java/com/a702/sarkem/service/GameManager.java
@@ -142,7 +142,7 @@ public class GameManager {
 			// 방장 변경하기
 			setHostId(roomId, nextHost);
 			sendChangeHostMessage(roomId, nextHost);
-			sendNoticeMessageToPlayer(roomId, nextHost, "호스트가 귀하로 변경되었습니다.", PhaseType.READY);
+			sendNoticeMessageToPlayer(roomId, nextHost, "호스트가 귀하로 변경되었습니다.", PhaseType.NIGHT); // TODO ready페이즈로 변경해야함
 			System.out.println("방장이 변경되었습니다." + playerId + " " + getHostId(roomId));
 		}
 	}
@@ -241,7 +241,7 @@ public class GameManager {
 		// 회의 시간 변경
 		int meetingTime = option.getMeetingTime();
 		if (meetingTime < 15 || meetingTime > 180) {
-			sendNoticeMessageToPlayer(roomId, hostId, "회의 시간은 15s ~ 180s 사이로 설정 가능합니다.", PhaseType.READY);
+			sendNoticeMessageToPlayer(roomId, hostId, "회의 시간은 15s ~ 180s 사이로 설정 가능합니다.", PhaseType.NIGHT); // TODO ready페이즈로 변경해야함
 			return;
 		}
 
@@ -283,7 +283,7 @@ public class GameManager {
 
 
 		if (playerCount != optionRoleCount) {
-			sendNoticeMessageToPlayer(roomId, playerId, "플래이어 수와 역할 수가 일치하지 않습니다.", PhaseType.READY);
+			sendNoticeMessageToPlayer(roomId, playerId, "플래이어 수와 역할 수가 일치하지 않습니다.", PhaseType.NIGHT);// TODO ready페이즈로 변경해야함
 			return false;
 		} 
 		// TODO: 테스트를 위해 아래 elif문 일단 주석, 나중에 주석 풀어야함
@@ -293,17 +293,17 @@ public class GameManager {
 //		}
 		// 삵이 한명인데 탐정이 있으면 안됨
 		else if(gameSession.getSarkCount()==1&&gameSession.getDetectiveCount()>0) { 
-			sendNoticeMessageToPlayer(roomId, playerId, "삵이 2명 이상일 때 탐정 직업을 가질 수 있습니다.", PhaseType.READY);
+			sendNoticeMessageToPlayer(roomId, playerId, "삵이 2명 이상일 때 탐정 직업을 가질 수 있습니다.", PhaseType.NIGHT);// TODO ready페이즈로 변경해야함
 			return false;
 		}
 		// 삵이 아예 없으면 안됨
 		else if(gameSession.getSarkCount()==0) { 
-			sendNoticeMessageToPlayer(roomId, playerId, "삵이 1명 이상 있어야합니다.", PhaseType.READY);
+			sendNoticeMessageToPlayer(roomId, playerId, "삵이 1명 이상 있어야합니다.", PhaseType.NIGHT);// TODO ready페이즈로 변경해야함
 			return false;
 		}
 		// 삵이 시민팀 수보다 많거나 같으면 안됨
 		else if(gameSession.getSarkCount()>=(playerCount+1)/2) { 
-			sendNoticeMessageToPlayer(roomId, playerId, "삵은 시민팀 수보다 적어야합니다.", PhaseType.READY);
+			sendNoticeMessageToPlayer(roomId, playerId, "삵은 시민팀 수보다 적어야합니다.", PhaseType.NIGHT);// TODO ready페이즈로 변경해야함
 			return false;
 		}
 		return true;
