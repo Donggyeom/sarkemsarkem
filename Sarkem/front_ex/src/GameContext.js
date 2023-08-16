2e6ece6267363798d07c0e8f9602327aa8b6547e
hyunchul1998 <hyunchul1998@naver.com>
2023-08-16 17:33:50 +0900

    Fix: 제밟제발

--- Sarkem/front_ex/src/GameContext.js
+++ Sarkem/front_ex/src/GameContext.js
@@ -576,7 +576,7 @@ const GameProvider = ({ children }) => {
 
     case "LEAVE_PLAYER":
         console.log(sysMessage.param);
-        players.current.delete(systemMessages.param);
+        players.current.delete(sysMessage.param);
         console.log(players.current);
         forceUpdate();
         break;
